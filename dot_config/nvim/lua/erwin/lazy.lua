3dff45471d986b3e642385963402630593ce5493
DjCoke <8378780+DjCoke@users.noreply.github.com>
2026-01-11 20:17:32 +0100

    Change attributes of .config/nvim/lua/erwin/lazy.lua

--- dot_config/nvim/lua/erwin/lazy.lua
+++ dot_config/nvim/lua/erwin/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("erwin.plugins")
+require("lazy").setup("erwin.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
