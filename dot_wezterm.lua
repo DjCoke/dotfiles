16b0a0c87f0eab12ae2683537484477843e0e30d
DjCoke <8378780+DjCoke@users.noreply.github.com>
2026-01-13 21:55:37 +0100

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,17 +5,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
-
-config.enable_tab_bar = false
-
-config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
 -- my coolnight colorscheme:
 config.colors = {
 	foreground = "#CBE0F0",
@@ -28,6 +17,16 @@ config.colors = {
 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 19
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
